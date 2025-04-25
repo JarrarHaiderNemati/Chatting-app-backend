9319b5efea9df883fb7ad66eb4dccdf969b76e19
Jarrar <jarrarnemati@gmail.com>
2025-04-25 16:04:00 +0500

    Added mongo_uri link # 2

--- server.js
+++ server.js
@@ -31,9 +31,10 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log("Mongo URI:", process.env.MONGO_URI); // Add this
+  console.log("Mongo URI:", process.env.MONGO_URI); 
   console.log("✅ Connected to MongoDB");
 }).catch((err) => {
+  console.log("Mongo URI:", process.env.MONGO_URI); // Add this
   console.error("❌ MongoDB connection failed:", err);
 });
 
